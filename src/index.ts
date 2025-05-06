122694fd782113c450b4a578b5b22ca626e1fee3
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-05-06 14:01:25 +0200

    v.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { bot } from "./bot/bot";
+import Elysia from "elysia";
 
+const app = new Elysia();
+app.get("/", () => "Hello World!");
+app.get("/health", () => "OK");
 
+app.listen(9000, () => {
+  console.log("Server is running!");
+});
 
 bot.launch().then(() => console.log("Bot is runnings!"));
 process.once('SIGINT', () => bot.stop('SIGINT'))
